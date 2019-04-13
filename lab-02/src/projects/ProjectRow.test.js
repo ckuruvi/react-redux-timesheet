39616f5f208c88bd6ad300f32c4a51510e471d58
Charles Kuruvila <charleskuruvila@MSP22CCK0820.local>
2019-04-13 11:11:47 -0500

    extra credit

--- lab-02/src/projects/ProjectRow.test.js
+++ lab-02/src/projects/ProjectRow.test.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import ProjectRow from './ProjectRow';
 
 describe('<ProjectRow />', () => {
-  // TODO Add tests
+    let wrapper;
+
+    beforeEach(() => {
+        const project = {
+            name: 'NAME',
+            description: 'DESCRIPTION'
+        };
+        wrapper = shallow(<ProjectRow project={project} />);
+    });
+
+    it('should instantiate the project row component', () =>{
+        expect(wrapper).toHaveLength(1)
+    })
+
+    it('should render values into expected calls', () => {
+        expect(wrapper.find('td')).toHaveLength(2)
+    })
+
+    it('renders correctly', () => {
+        const project = {
+            name: 'NAME',
+            description: 'DESCRIPTION'
+        };
+        const tree = renderer.create(<ProjectRow project={project} />).toJSON()
+        expect(tree).toMatchSnapshot();
+    })
+
 });
